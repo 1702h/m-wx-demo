34be17434cf8e0feafbe42117352ece6c8da50d4
xutongbao <1183391880@qq.com>
2019-07-17 09:45:07 +0800

    通讯录

--- m-tab-bar/pages/list/list.js
+++ m-tab-bar/pages/list/list.js
@@ -5,7 +5,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    list: [],
+    currentKey:'A',
+    currentIndex: 0,
+    listItemTops: []
   },
 
   /**
@@ -19,7 +22,42 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
+    wx.request({
+      url: "https://www.easy-mock.com/mock/5ceb8bac32cfe337f96fe748/example/car",
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          list: res.data.data.items
+        }, () => {
+          const query = wx.createSelectorQuery();
+          query.selectAll('.js-list-item').boundingClientRect((res) => {
+            console.log(res)
+            this.setData({
+              listItemTops: res.map(item => item.top)
+            })
+          }).exec()
+        })
+      }
+    })
+  },
+  handleScroll(e) {
+    let scrollTop = e.detail.scrollTop
+    let {listItemTops} = this.data
+    for (let i = 0; i < listItemTops.length; i++) {
+      if (scrollTop >= listItemTops[i]) {
+        this.setData({
+          currentIndex: i
+        })
+      }
+    }
 
+  },
+
+  handleNav(e) {
+    let {key} = e.currentTarget.dataset
+    this.setData({
+      currentKey: key
+    })
   },
 
   /**
